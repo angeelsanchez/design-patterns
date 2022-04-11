b2b93934e7f2e4501f708c5830763f0842f4dc3a
Angel David Sanchez <angel.da@hotmail.es>
2022-04-11 23:01:47 +0200

    controle some exceptions in Aplication

--- editor/src/main/java/com/kreitek/editor/Application.java
+++ editor/src/main/java/com/kreitek/editor/Application.java
@@ -1,17 +1,21 @@
 package com.kreitek.editor;
 
-import com.kreitek.editor.editor.JsonEditor;
-import com.kreitek.editor.interfaces.Editor;
 import com.kreitek.editor.editor.EditorFactory;
 
 public class Application {
 
     public static void main(String[] args) {
         EditorFactory editorFactory = new EditorFactory();
-
-        if (args[0].equals("json"))
-            editorFactory.getJsonEditor().run();
-        else if (args[0].equals("text"))
-            editorFactory.getTextEditor().run();
+        if (args.length == 1) {
+            if (args[0].equals("json"))
+                editorFactory.getJsonEditor().run();
+            else if (args[0].equals("text"))
+                editorFactory.getTextEditor().run();
+            else {
+                System.out.println("You must enter json or text");
+            }
+        } else {
+            System.out.println("You must enter a parameter");
+        }
     }
 }
