9d7e32d063e25f18ba8bf6a8ad0eca17b79ec3ae
Angel David Sanchez <angel.da@hotmail.es>
2022-04-11 20:30:55 +0200

    update: EditorFactory

--- editor/src/main/java/com/kreitek/editor/editor/EditorFactory.java
+++ editor/src/main/java/com/kreitek/editor/editor/EditorFactory.java
@@ -3,5 +3,11 @@ package com.kreitek.editor.editor;
 import com.kreitek.editor.interfaces.Editor;
 
 public class EditorFactory {
+    public Editor getTextEditor() {
+        return new TextEditor();
+    }
 
+    public Editor getJsonEditor() {
+        return new JsonEditor();
+    }
 }
