fdfd4eeb0e581516edd3aa5766bc99aa4b69e8a9
Angel David Sanchez <angel.da@hotmail.es>
2022-04-11 20:38:11 +0200

    update Aplication

--- editor/src/main/java/com/kreitek/editor/Application.java
+++ editor/src/main/java/com/kreitek/editor/Application.java
@@ -7,5 +7,11 @@ import com.kreitek.editor.editor.EditorFactory;
 public class Application {
 
     public static void main(String[] args) {
+        EditorFactory editorFactory = new EditorFactory();
+
+        if (args[0].equals("json"))
+            editorFactory.getJsonEditor().run();
+        else if (args[0].equals("text"))
+            editorFactory.getTextEditor().run();
     }
 }
